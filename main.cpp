397e0ec5a727f4998366d26521307c050a4e89a3
Giane Barreto <123519846+GianellaTBarreto@users.noreply.github.com>
2025-07-22 17:37:48 -0300

    main nuevo

--- main.cpp
+++ main.cpp
@@ -1,77 +1,118 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-class jugador {
-private:
-    string nombre;
+enum class Personaje { ROJO, AZUL, AMARILLO, VERDE };
 
+enum class TipoCasilla {
+    NORMAL,
+    CASILLA_DE_OCA,
+    PUENTE,
+    POSADA,
+    POZO,
+    LABERINTO,
+    CARCEL,
+    CALAVERA,
+    JARDIN_DE_OCA
+};
+
+class Tablero{
+protected:
+    Jugador jugadores[4];
+    Casilla casillas[63];
 public:
-    int posicion;
-    int dados=0;
+    void iniciarJuego(){
 
-    //constructor
-    jugador(string n) {
-        nombre = n;
     }
 
-    int tirarDados(int dados){
-        dados= rand() % 6 + 1;
-        return dados;
+    Casilla* obtenerCasilla(int pos){
+
     }
 
-    int cambiarPosicion(int posicion){
-        posicion += dados;
-        return posicion;
+    void moverJugador(Jugador *jugadores, int pasos){
+
     }
 };
 
 class Casilla {
+protected:
+    int pos_;
 public:
-    int pos;
+    explicit Casilla(int pos) : pos_(pos) {}
+    virtual ~Casilla() = default;
 
-    virtual int realizarAccion();
+    virtual int realizarAccion(Jugador& jugador) = 0; // devuelve nueva posición
+    int getPos() const { return pos_; }
+};
+
+class CasillaNormal : public Casilla {
+public:
+    using Casilla::Casilla;
+    int realizarAccion(Jugador&) override { return pos_; }
 };
 
 class CasillaEspecial : public Casilla {
+    TipoCasilla tipo_;
 public:
-    int realizarAccion(pos){
-        switch (pos){
-        case 9: pos = 18;
-            break;
-        case 18: pos = 27;
-            break;
-        case 27: pos = 36;
-            break;
-        case 36: pos = 45;
-            break;
-        case 45: pos = 54;
-            break;
-        }
+    CasillaEspecial(int pos, TipoCasilla tipo)
+        : Casilla(pos), tipo_(tipo) {}
 
-    }
+    int realizarAccion(Jugador& jugador) override;
+    TipoCasilla getTipo() const { return tipo_; }
 };
 
-class tablero {
-private:
-    casilla tableros[63];
-    jugador* jugadores[4];
+int CasillaEspecial::realizarAccion(Jugador& jugador){
+    switch (_tipo) {
+    case tipoCasilla::PUENTE:
+        cout<<"Del puente a la posada. Avanza a la casilla 12."<<endl;
+        return 12;
+        break;
+    default:
+        break;
+    }
+}
+
+class Jugador {
+    Personaje color_;
+    string    nombre_;
+    int       posicion_{0};
+    Tablero  *tablero_;
+
+    static int tirarDadosInterno() {
+        static random_device rd;
+        static mt19937 gen(rd());
+        static uniform_int_distribution<int> dist(1, 6);
+        return dist(gen);
+    }
 
 public:
-    tablero() {
-        jugadores[0] = new Jugador("Jim");
-        jugadores[1] = new Jugador("Pam");
-        jugadores[2] = new Jugador("Dwight");
-        jugadores[3] = new Jugador("Michael");
+    Jugador(Personaje c, string nom, Tablero* t)
+        : color_(c), nombre_(move(nom)), tablero_(t) {}
+
+    int  tirarDados()                 { return tirarDadosInterno(); }
+    void cambiarPosicion(int nueva)   { posicion_ = nueva; }
+
+    void jugarTurno() {
+        int d = tirarDados();
+        cout << nombre_ << " tiró " << d << '\n';
+        tablero_->moverJugador(*this, d);
+        cout << nombre_ << " está en " << posicion_ << '\n';
     }
+
+    int  getPos() const { return posicion_; }
+    const string& getNombre() const { return nombre_; }
 };
+
+
+class Personaje :
 
 
 
 int main()
 {
-   srand(time(0));
+    cout << "Hello World!" << endl;
     return 0;
 }
